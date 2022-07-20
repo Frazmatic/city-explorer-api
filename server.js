42fd7142e7043e478e9de35095dc5ddc7dbf7dac
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-07-19 17:53:39 -0700

    working for lab 7

--- server.js
+++ server.js
@@ -12,32 +12,47 @@ class Forecast {
     this.date = date;
     this.description = description;
   }
+
+  static makeForecasts(city) {
+    const forecasts = city.data.map((d) => {
+      const date = d.valid_date;
+      const description =
+        `Low of ${d.low_temp}, ` +
+        `high of ${d.high_temp} ` +
+        `with ${d.weather.description}`;
+      return new Forecast(date, description);
+    });
+    return forecasts;
+  }
+}
+
+function findCity(name, lat, lon, weatherData) {
+  console.log('finding city');
+  try {
+    const city = weatherData.find((el) => {
+      return (
+        name === el.city_name &&
+        // the lat & lon in our sample data and 
+        // the locationiq are slightly different
+        Math.round(lat) === Math.round(el.lat) &&
+        Math.round(lon) === Math.round(el.lon)
+      );
+    });
+    return city;
+  } catch (err) {
+    throw new Error('No City Match in Weather Data' + err);
+  }
 }
 
 // weather endpoint
 server.get('/weather', (req, res) => {
   const queries = req.query;
-  const city = weather.find((el) => {
-    return (
-      queries.city_name === el.city_name &&
-      queries.lat === el.lat &&
-      queries.lon === el.lon
-    );
-  });
-
-  const forecasts = city.data.map((d) => {
-    const date = d.valid_date;
-    const description =
-      `Low of ${d.low_temp}, ` +
-      `high of ${d.high_temp} ` +
-      `with ${d.weather.description}`;
-    return new Forecast(date, description);
-  });
+  const city = findCity(queries.city_name, queries.lat, queries.lon, weather);
 
   if (city) {
-    res.send(forecasts);
+    res.send(Forecast.makeForecasts(city));
   } else {
-    res.status(404).send('City not found');
+    res.status(404).send('City not found in weather data');
   }
 });
 
